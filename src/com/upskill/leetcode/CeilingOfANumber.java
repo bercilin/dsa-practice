736e91c9f49eb83926a0f40461c1f0ec62c080e5
Bercilin Jose <bercilinjose07@gmail.com>
2025-04-10 22:13:00 +0530

    Celing and floor of a number

---
+++ src/com/upskill/leetcode/CeilingOfANumber.java
@@ -0,0 +1,34 @@
+package com.upskill.leetcode;
+
+public class CeilingOfANumber {
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5, 8, 10, 14, 18, 23 };
+		int target = 17;
+
+		int result = findCelingOfANumber(arr, target);
+		System.out.println("result: " + result);
+
+	}
+
+	private static int findCelingOfANumber(int[] arr, int target) {
+
+		int start = 0;
+		int end = arr.length - 1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (target == arr[mid]) {
+				return mid;
+			}
+
+			if (target > arr[mid]) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+
+		return arr[start];
+	}
+
+}
